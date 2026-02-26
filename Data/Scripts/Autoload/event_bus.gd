aba3756f1d1716627b543d83c1dd55eca5f9488e
Lita Lavender <baron11704@yahoo.com>
2026-02-25 22:48:59 -0500

    Floors

--- Data/Scripts/Autoload/event_bus.gd
+++ Data/Scripts/Autoload/event_bus.gd
@@ -2,9 +2,13 @@ extends Node
 
 signal room_exited(room:Room, direction: Room.Directions)
 signal enemy_died(enemy: Enemy)
+signal next_floor(new_floor: int)
 
 func _on_room_exited(room: Room, direction: Room.Directions):
 	room_exited.emit(room,direction)
 
 func _on_enemy_died(enemy: Enemy):
 	enemy_died.emit(enemy)
+
+func _on_next_floor(new_floor: int):
+	next_floor.emit(new_floor)
--- Data/Scripts/level.gd
+++ Data/Scripts/level.gd
@@ -7,22 +7,28 @@ class_name Level
 @export var level_camera: Camera2D
 
 var remaining_rooms: Array[PackedScene]
-var remaining_boss_rooms: Array[PackedScene]
 
 const ROOM_OFFSET = Vector2(623,353)
 
-func get_remaining_rooms(boss_rooms := false):
+signal next_floor(new_floor: int)
+
+func get_remaining_rooms(boss_rooms := false) -> Array[PackedScene]:
+	var array: Array[PackedScene] = []
+	var folder_path: String
 	if !boss_rooms:
-		var folder_path = "Scenes/Rooms/Floor "+str(current_floor)
-		print(folder_path)
-		var folder := DirAccess.open(folder_path)
-		for file_name in folder.get_files():
-			remaining_rooms.append(load(folder_path+"/"+file_name))
+		folder_path = "Scenes/Rooms/Floor "+str(current_floor)
+	else:
+		folder_path = "Scenes/Rooms/Floor "+str(current_floor)+"/Bosses"
+	var folder := DirAccess.open(folder_path)
+	for file_name in folder.get_files():
+		array.append(load(folder_path+"/"+file_name))
+	return array
 
 func _ready():
-	get_remaining_rooms()
-	get_remaining_rooms(true)
+	remaining_rooms = get_remaining_rooms()
 	E.room_exited.connect(_on_room_exited)
+	next_floor.connect(E._on_next_floor)
+	E.next_floor.connect(_on_next_floor)
 
 
 func _on_room_exited(room: Room, direction: Room.Directions):
@@ -31,29 +37,38 @@ func _on_room_exited(room: Room, direction: Room.Directions):
 		var player: Player = find_child("Player")
 		player.reset_current_combo_life()
 	current_room += 1
+	var new_room: Room
 	if current_room == 3:
-		pass
+		var new_room_scene = load("res://Scenes/Rooms/healing.tscn")
+		new_room = new_room_scene.instantiate()
 	elif current_room == 6:
-		pass
+		var potential_bosses: Array[PackedScene] = get_remaining_rooms(true)
+		new_room = potential_bosses.pick_random().instantiate()
 	else:
 		if !remaining_rooms:
 			push_error("OUT OF ROOMS")
 		var new_room_scene = remaining_rooms.pick_random()
-		var new_room: Room = new_room_scene.instantiate()
+		new_room = new_room_scene.instantiate()
 		remaining_rooms.erase(new_room_scene)
-		print(direction)
-		print("New direction "+str(direction + 2 % 4))
-		new_room.entrance_direction = (direction + 2) % 4 as Room.Directions
-		call_deferred("add_child",new_room)
-		var point_vector: Vector2
-		match direction:
-			Room.Directions.NORTH: point_vector = Vector2.UP
-			Room.Directions.SOUTH: point_vector = Vector2.DOWN
-			Room.Directions.EAST: point_vector = Vector2.RIGHT
-			Room.Directions.WEST: point_vector = Vector2.LEFT
-		new_room.position = current_room_node.position + ROOM_OFFSET*point_vector
-		current_room_node.leave()
-		current_room_node = new_room
-		var tween = create_tween()
-		tween.set_ease(Tween.EASE_IN_OUT)
-		tween.tween_property(level_camera,"global_position",level_camera.global_position + ROOM_OFFSET*point_vector,1.0)
+		#print(direction)
+		#print("New direction "+str(direction + 2 % 4))
+	new_room.entrance_direction = (direction + 2) % 4 as Room.Directions
+	call_deferred("add_child",new_room)
+	var point_vector: Vector2
+	match direction:
+		Room.Directions.NORTH: point_vector = Vector2.UP
+		Room.Directions.SOUTH: point_vector = Vector2.DOWN
+		Room.Directions.EAST: point_vector = Vector2.RIGHT
+		Room.Directions.WEST: point_vector = Vector2.LEFT
+	new_room.position = current_room_node.position + ROOM_OFFSET*point_vector
+	current_room_node.leave()
+	current_room_node = new_room
+	var tween = create_tween()
+	tween.set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(level_camera,"global_position",level_camera.global_position + ROOM_OFFSET*point_vector,1.0)
+	if current_room == 7:
+		current_room = 1
+		next_floor.emit(current_floor + 1)
+
+func _on_next_floor(new_floor: int):
+	current_floor = new_floor
