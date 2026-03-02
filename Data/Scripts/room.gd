78da625cc985c22c816e8367ef2c21664a614ee9
Lita Lavender <baron11704@yahoo.com>
2026-03-02 17:34:39 -0500

    South Exit bug fix + Doors

--- Data/Scripts/room.gd
+++ Data/Scripts/room.gd
@@ -10,7 +10,18 @@ enum Directions {
 
 @export var has_enemies := true
 
-
+var door_atlas_coords: Dictionary[Directions, Vector2i] = {
+	Directions.NORTH: Vector2i(3,3),
+	Directions.SOUTH: Vector2i(3,2),
+	Directions.WEST: Vector2i(2,2),
+	Directions.EAST: Vector2i(2,3),
+}
+var door_tile_coords: Dictionary[Directions, Vector2i] = {
+	Directions.NORTH: Vector2i(-1,-7),
+	Directions.SOUTH: Vector2i(-1,3),
+	Directions.WEST: Vector2i(-10,-2),
+	Directions.EAST: Vector2i(8,-2),
+}
 var tokens_on_clear: int = 0
 var inverted := false
 var entrance_direction: Directions:
@@ -23,11 +34,13 @@ var special_exit_direction: Directions:
 		special_exit_direction = value
 		var marker_name = Directions.find_key(special_exit_direction).capitalize()+"Marker"
 		find_child(marker_name).visible = true
+
+@onready var wall_tilemap: TileMapLayer = find_child("OuterWall")
 @onready var player: Player = get_tree().get_first_node_in_group("Player")
 @onready var enemies: Node2D = find_child("Enemies")
 
 const TRANSITION_TIME = 1.0
-const ENTRANCE_PLACEMENT_OFFSET = 20
+const ENTRANCE_PLACEMENT_OFFSET = 32
 
 signal room_exited(room: Room,direction: Room.Directions)
 signal room_cleared(room: Room)
@@ -36,22 +49,40 @@ signal acquire_token(amount: int)
 func leave():
 	queue_free()
 
+func close_doors():
+	for direction in Directions:
+		direction = Directions[direction]
+		if !wall_tilemap.get_cell_tile_data(door_tile_coords[direction]):
+			wall_tilemap.set_cell(door_tile_coords[direction],0,door_atlas_coords[direction])
+
+func open_doors():
+	for direction in Directions:
+		direction = Directions[direction]
+		if wall_tilemap.get_cell_atlas_coords(door_tile_coords[direction]) == door_atlas_coords[direction]:
+			wall_tilemap.erase_cell(door_tile_coords[direction])
+
+
 func _ready():
 	room_exited.connect(E._on_room_exited)
 	room_cleared.connect(E._on_room_cleared)
 	acquire_token.connect(E._on_acquire_token)
 	E.enemy_died.connect(_on_enemy_died)
-	if !entrance_direction: return
+	if entrance_direction == null or name == "StartingRoom":  
+		return
 	var entrance_player_position: Vector2
 	match entrance_direction:
 		Directions.NORTH: entrance_player_position = find_child("NorthExitArea").global_position + Vector2.DOWN * ENTRANCE_PLACEMENT_OFFSET
 		Directions.SOUTH: entrance_player_position = find_child("SouthExitArea").global_position + Vector2.UP * ENTRANCE_PLACEMENT_OFFSET
 		Directions.WEST: entrance_player_position = find_child("WestExitArea").global_position + Vector2.RIGHT * ENTRANCE_PLACEMENT_OFFSET
 		Directions.EAST: entrance_player_position = find_child("EastExitArea").global_position + Vector2.LEFT * ENTRANCE_PLACEMENT_OFFSET
+	print("Player Position: "+str(player.global_position))
+	print("Entrance Player Position: "+str(entrance_player_position))
 	G.halt_actions = true
 	entrance_tween = create_tween()
 	entrance_tween.set_ease(Tween.EASE_OUT)
 	entrance_tween.tween_property(player,"global_position",entrance_player_position,TRANSITION_TIME)
+	if has_enemies:
+		entrance_tween.tween_callback(close_doors)
 	entrance_tween.tween_property(G,"halt_actions",false,0)
 	var usable_directions: Array[Directions] = [Directions.NORTH,Directions.SOUTH,Directions.EAST,Directions.WEST]
 	usable_directions.erase(entrance_direction)
@@ -65,6 +96,7 @@ func _room_cleared():
 	room_cleared.emit(self)
 	if tokens_on_clear != 0:
 		acquire_token.emit(tokens_on_clear)
+	open_doors()
 
 func _contract_effect():
 	print("Contract Effect")
